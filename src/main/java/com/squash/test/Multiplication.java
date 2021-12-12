783c4213b88f40f3f3dbfb77f22b988b8e71b004
srimaan-yarram <srimaan.yarram@coupa.com>
2021-12-12 12:51:22 -0800

    RSKDEV-002 Multiplication

---
+++ src/main/java/com/squash/test/Multiplication.java
@@ -0,0 +1,16 @@
+package com.squash.test;
+
+public class Multiplication {
+
+    public int multiplication (int a, int b) {
+        return a+b;
+    }
+
+    public int multiplication (int a, int b, int c, int d) {
+        return a+b+c+d;
+    }
+
+    public int multiplication (int a, int b, int c, int d, int e) {
+        return a+b+c+d+e;
+    }
+}
