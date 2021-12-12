c72c659eea9856921e926de4add6ac917f9903dd
Srimaan Yarram <srimaan@hotmail.com>
2021-12-12 12:43:08 -0800

    RSKDEV-001 5 Parameter Addition method (#1)
    RSKDEV-001 Removed 6 Parameter Addition method
    
    Co-authored-by: srimaan-yarram <srimaan.yarram@coupa.com>

--- src/main/java/com/squash/test/Addition.java
+++ src/main/java/com/squash/test/Addition.java
@@ -7,4 +7,16 @@ public class Addition {
     }
 
 
+    public int addition(int a, int b, int c) {
+        return a+b+c;
+    }
+
+    public int addition(int a, int b, int c, int d) {
+        return a+b+c+d;
+    }
+
+    public int addition(int a, int b, int c, int d, int e) {
+        return a+b+c+d+e;
+    }
+
 }
