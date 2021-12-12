09f689c1626a6ac2062a3070111318218e742bbb
srimaan-yarram <srimaan.yarram@coupa.com>
2021-12-12 12:45:16 -0800

    RSKDEV-001 Removed unnecessary addition method

--- src/main/java/com/squash/test/Addition.java
+++ src/main/java/com/squash/test/Addition.java
@@ -6,17 +6,12 @@ public class Addition {
         return a+b;
     }
 
-
     public int addition(int a, int b, int c) {
         return a+b+c;
     }
 
     public int addition(int a, int b, int c, int d) {
         return a+b+c+d;
-    }
-
-    public int addition(int a, int b, int c, int d, int e) {
-        return a+b+c+d+e;
     }
 
 }
