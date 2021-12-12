2d410f53b2e5d1821d86bf3a8456e6224d925780
srimaan-yarram <srimaan.yarram@coupa.com>
2021-12-12 12:53:08 -0800

    RSKDEV-002 Fixed Bug addtition to Multiplication

--- src/main/java/com/squash/test/Multiplication.java
+++ src/main/java/com/squash/test/Multiplication.java
@@ -3,7 +3,11 @@ package com.squash.test;
 public class Multiplication {
 
     public int multiplication (int a, int b) {
-        return a+b;
+        return a*b;
+    }
+
+    public int multiplication (int a, int b, int c) {
+        return a*b*c;
     }
 
 }
