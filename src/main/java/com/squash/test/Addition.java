e5fd295f143a67992d1b26f366378053e4118baa
srimaan-yarram <srimaan.yarram@coupa.com>
2021-12-12 12:40:14 -0800

    RSKDEV-001 4 Parameter Addition method

--- src/main/java/com/squash/test/Addition.java
+++ src/main/java/com/squash/test/Addition.java
@@ -10,4 +10,11 @@ public class Addition {
     public int addition(int a, int b, int c) {
         return a+b+c;
     }
+
+    public int addition(int a, int b, int c, int d) {
+        return a+b+c+d;
+    }
+
+
+
 }
